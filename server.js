d88a318b65778976821a4276b2f2d32e5d29dc48
Joao Martins <joaomartins@MBP-de-Joao.home>
2024-07-02 19:49:51 +0100

    created articles router and started creating index view

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const articleRouter = require("./routes/articles");
+
 const app = express();
+
 app.listen(5500);
+
+app.use("/articles", articleRouter);
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const articles = [
+    {
+      title: "test",
+      description: "test",
+      markdown: "test",
+      createdAt: new Date(),
+    },
+    {
+      title: "test2",
+      description: "test2",
+      markdown: "test2",
+      createdAt: new Date(),
+    },
+  ];
+  res.render("index", { articles: articles });
 });
