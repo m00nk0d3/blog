e5050366916222727ea76dec54c4e5a87bf7535a
Joao Martins <joaomartins@MBP-de-Joao.home>
2024-07-03 17:40:23 +0100

    more development
    added slugify and method override to the project

--- routes/articles.js
+++ routes/articles.js
@@ -7,9 +7,14 @@ router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
 
-router.get("/:id", async (req, res) => {
-  const article = await Article.findById(req.params.id);
+router.get("/:slug", async (req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
   res.render("articles/show", { article: article });
+});
+
+router.delete("/:id", async (req, res) => {
+  await Article.findByIdAndDelete(req.params.id);
+  res.redirect("/");
 });
 
 router.post("/", async (req, res) => {
@@ -20,7 +25,7 @@ router.post("/", async (req, res) => {
   });
   try {
     article = await article.save();
-    res.redirect(`/articles/${article.id}`);
+    res.redirect(`/articles/${article.slug}`);
   } catch (e) {
     console.log(e);
     res.render("articles/new", { article: article });
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const articleRouter = require("./routes/articles");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const app = express();
 const Article = require("./models/article");
 
 app.listen(5500);
 
 mongoose.connect("mongodb://localhost/blog");
-
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/articles", articleRouter);
